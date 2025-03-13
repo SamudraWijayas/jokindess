a849e1ac38a8fb6bc23897a695c9e9767e8f2147
SamudraWijayas <swijayasamdoria@gmail.com>
2025-03-13 15:16:30 +0700

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
       <h1 className="text-6xl font-bold">404</h1>
       <p className="text-xl mt-2">Halaman belum tersedia</p>
-      <a
+      <Link
         href="/"
         className="mt-4 px-6 py-3 bg-green-600 text-white font-bold rounded-full hover:bg-green-700 transition"
       >
         Kembali ke Beranda
-      </a>
+      </Link>
     </div>
   );
 }
